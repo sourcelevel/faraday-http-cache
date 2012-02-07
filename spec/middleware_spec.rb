758098aea1d33765ed6afb8d5ee32208ea9967f6
Lucas Mazza <lucas.mazza@plataformatec.com.br>
2012-02-07 14:50:09 -0200

    add a test for the no-store directive

--- spec/middleware_spec.rb
+++ spec/middleware_spec.rb
@@ -7,10 +7,11 @@ describe Faraday::HttpCache::Middleware do
       stack.use Faraday::HttpCache::Middleware
 
       stack.adapter :test do |stubs|
-        stubs.post('post')   { [200, { 'Cache-Control' => 'max-age=400' }, "post:#{@post_count+=1}"] }
-        stubs.get('broken')  { [500, {'Cache-Control' => 'max-age=400' },  "broken:#{@broken_count+=1}"] }
-        stubs.get('get')     { [200, {'Cache-Control' => 'max-age=200' },  "get:#{@get_count+=1}"] }
-        stubs.get('private') { [200, {'Cache-Control' => 'private' },      "get:#{@get_count+=1}"] }
+        stubs.post('post')     { [200, { 'Cache-Control' => 'max-age=400' }, "post:#{@post_count+=1}"] }
+        stubs.get('broken')    { [500, {'Cache-Control' => 'max-age=400' },  "broken:#{@broken_count+=1}"] }
+        stubs.get('get')       { [200, {'Cache-Control' => 'max-age=200' },  "get:#{@get_count+=1}"] }
+        stubs.get('private')   { [200, {'Cache-Control' => 'private' },      "get:#{@get_count+=1}"] }
+        stubs.get('dontstore') { [200, {'Cache-Control' => 'no-store' },  "get:#{@get_count+=1}"] }
       end
     end
   end
@@ -34,6 +35,11 @@ describe Faraday::HttpCache::Middleware do
   it "doesn't cache requests with a private cache control" do
     client.get('private')
     client.get('private').body.should == "get:2"
+  end
+
+  it "doesn't cache requests with a explicit no-store directive" do
+    client.get('dontstore')
+    client.get('dontstore').body.should == "get:2"
   end
 
   it "caches GET responses" do
