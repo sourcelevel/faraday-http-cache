463dd80bf79b4ce53d6e4fbea69563894810c06b
Lucas Mazza <lucas.mazza@plataformatec.com.br>
2012-02-07 14:56:05 -0200

    trim up this instance variables madness

--- spec/middleware_spec.rb
+++ spec/middleware_spec.rb
@@ -7,49 +7,47 @@ describe Faraday::HttpCache::Middleware do
       stack.use Faraday::HttpCache::Middleware
 
       stack.adapter :test do |stubs|
-        stubs.post('post')     { [200, { 'Cache-Control' => 'max-age=400' }, "post:#{@post_count+=1}"] }
-        stubs.get('broken')    { [500, {'Cache-Control' => 'max-age=400' },  "broken:#{@broken_count+=1}"] }
-        stubs.get('get')       { [200, {'Cache-Control' => 'max-age=200' },  "get:#{@get_count+=1}"] }
-        stubs.get('private')   { [200, {'Cache-Control' => 'private' },      "get:#{@get_count+=1}"] }
-        stubs.get('dontstore') { [200, {'Cache-Control' => 'no-store' },  "get:#{@get_count+=1}"] }
-        stubs.get('expires')   { [200, {'Expires' => (Time.now + 10).httpdate }, "get:#{@get_count+=1}"]}
+        stubs.post('post')     { [200, { 'Cache-Control' => 'max-age=400' }, "#{@request_count+=1}"] }
+        stubs.get('broken')    { [500, {'Cache-Control' => 'max-age=400' },  "#{@request_count+=1}"] }
+        stubs.get('get')       { [200, {'Cache-Control' => 'max-age=200' },  "#{@request_count+=1}"] }
+        stubs.get('private')   { [200, {'Cache-Control' => 'private' },      "#{@request_count+=1}"] }
+        stubs.get('dontstore') { [200, {'Cache-Control' => 'no-store' },  "#{@request_count+=1}"] }
+        stubs.get('expires')   { [200, {'Expires' => (Time.now + 10).httpdate }, "#{@request_count+=1}"]}
       end
     end
   end
 
   before do
-    @post_count = 0
-    @broken_count = 0
-    @get_count = 0
+    @request_count = 0
   end
 
   it "doesn't cache POST requests" do
     client.post('post').body
-    client.post('post').body.should == "post:2"
+    client.post('post').body.should == "2"
   end
 
   it "doesn't cache responses with invalid status code" do
     client.get('broken')
-    client.get('broken').body.should == "broken:2"
+    client.get('broken').body.should == "2"
   end
 
   it "doesn't cache requests with a private cache control" do
     client.get('private')
-    client.get('private').body.should == "get:2"
+    client.get('private').body.should == "2"
   end
 
   it "doesn't cache requests with a explicit no-store directive" do
     client.get('dontstore')
-    client.get('dontstore').body.should == "get:2"
+    client.get('dontstore').body.should == "2"
   end
 
   it "caches requests with the 'Expires' header" do
     client.get('expires')
-    client.get('expires').body.should == "get:1"
+    client.get('expires').body.should == "1"
   end
 
   it "caches GET responses" do
     client.get('get')
-    client.get('get').body.should == "get:1"
+    client.get('get').body.should == "1"
   end
 end
