f5368df9c30e6f65bceefe74b2f2837d5f1191d2
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2012-03-03 20:53:07 -0300

    Define the module in the proper file

--- lib/faraday/http_cache.rb
+++ lib/faraday/http_cache.rb
@@ -5,6 +5,11 @@ require 'faraday/http_cache/storage'
 require 'faraday/http_cache/response'
 require 'faraday/http_cache/middleware'
 
+module Faraday
+  module HttpCache
+  end
+end
+
 if Faraday.respond_to?(:register_middleware)
   Faraday.register_middleware :http_cache => lambda { Faraday::HttpCache::Middleware }
 end
