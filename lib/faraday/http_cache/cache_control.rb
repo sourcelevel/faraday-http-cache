b08c14849f1e561378e9f54aaf6a7daa235f66df
Lucas Mazza <lucas.mazza@plataformatec.com.br>
2012-03-14 18:50:08 -0300

    documenting the CacheControl

--- lib/faraday/http_cache/cache_control.rb
+++ lib/faraday/http_cache/cache_control.rb
@@ -2,52 +2,72 @@ require 'forwardable'
 
 module Faraday
   module HttpCache
-    # A parser for the `Cache-Control` header, based on the implementation done
-    # in the `rack-cache` gem by Ryan Tomayko (https://github.com/rtomayko/rack-cache/),
-    # Storing a `Hash` instead of inheriting from it.
+    # Internal: a class to represent the 'Cache-Control' header options.
+    #  This implementation is based on 'rack-cache' internals by Ryan Tomayko.
+    #  It breaks the several directives into keys/values and stores them into a
+    #  a Hash.
     class CacheControl
       extend Forwardable
 
       def_delegators :@directives, :[], :[]=, :include?
 
-      def initialize(string = nil)
+      # Internal: Initialize a new CacheControl.
+      def initialize(string)
         @directives = {}
         parse(string)
       end
 
+      # Internal: Checks if the 'public' directive is present
       def public?
         @directives['public']
       end
 
+      # Internal: Checks if the 'private' directive is present
       def private?
         @directives['private']
       end
 
+      # Internal: Checks if the 'no-cache' directive is present
       def no_cache?
         @directives['no-cache']
       end
 
+      # Internal: Checks if the 'no-store' directive is present
       def no_store?
         @directives['no-store']
       end
 
+      # Internal: Gets the 'max-age' directive as an Integer.
+      #
+      # Returns nil the 'max-age' directive isn't present.
       def max_age
         @directives['max-age'].to_i if @directives.key?('max-age')
       end
 
+      # Internal: Gets the 's-maxage' directive as an Integer.
+      #
+      # Returns nil the 's-maxage' directive isn't present.
       def shared_max_age
         @directives['s-maxage'].to_i if @directives.key?('s-maxage')
       end
       alias_method :s_maxage, :shared_max_age
 
+      # Internal: Checks if the 'must-revalidate' directive is present
       def must_revalidate?
         @directives['must-revalidate']
       end
 
+      # Internal: Checks if the 'proxy-revalidate' directive is present
       def proxy_revalidate?
         @directives['proxy-revalidate']
       end
 
+      # Internal: Gets the String representation for the cache directives.
+      #   Directives are joined by a '=' and then combined into a single String
+      #   separated by commas. Directives with a 'true' value will be omit the
+      #   '=' sign and their value.
+      #
+      # Returns the Cache Control string.
       def to_s
         booleans, values = [], []
 
@@ -64,6 +84,19 @@ module Faraday
 
       private
 
+      # Internal: Parses the Cache Control string into the directives Hash.
+      #  Existing whitespaces are removed, and the string is splited on commas.
+      #  For each segment, everything before a '=' will be treated as the key
+      #  and the excedding will be treated as the value. If only the key is
+      #  present the assigned value will defaults to true.
+      #
+      #  Examples:
+      #    "max-age=600"
+      #     # => { "max-age" => "600"}
+      #    "max-age"
+      #     # => { "max-age" => true }
+      #
+      # Returns nothing.
       def parse(string)
         string = string.to_s
 
