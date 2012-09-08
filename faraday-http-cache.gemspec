cec9e0041b2788797bc50957c2522b965b46e432
Lucas Mazza <lucas.mazza@plataformatec.com.br>
2012-09-08 12:10:33 -0300

    Oj segfaults like an idiot, so we use yajl-ruby on specs for now.

--- faraday-http-cache.gemspec
+++ faraday-http-cache.gemspec
@@ -18,4 +18,5 @@ Gem::Specification.new do |gem|
   gem.add_development_dependency 'rake'
   gem.add_development_dependency 'rspec', '~> 2.0'
   gem.add_development_dependency 'em-http-request', '~> 1.0.2'
+  gem.add_development_dependency 'yajl-ruby'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'uri'
 require 'faraday-http-cache'
 require 'active_support/core_ext/date/calculations'
 require 'active_support/core_ext/numeric/time'
+require 'yajl'
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
