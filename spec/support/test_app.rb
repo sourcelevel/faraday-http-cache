e3c2371b5550a134402d79f20a77a19c70cce2d5
Lucas Mazza <lucas.mazza@plataformatec.com.br>
2013-07-06 23:26:09 -0300

    Extract increment operation to a method

--- spec/support/test_app.rb
+++ spec/support/test_app.rb
@@ -21,36 +21,36 @@ class TestApp < Sinatra::Base
   end
 
   get '/json' do
-    json = MultiJson.encode(count: settings.requests += 1)
+    json = MultiJson.encode(count: increment_counter.to_i)
     [200, { 'Cache-Control' => 'max-age=400', 'Content-Type' => 'application/json' }, json]
   end
 
   post '/post' do
-    [200, { 'Cache-Control' => 'max-age=400' }, "#{settings.requests += 1}"]
+    [200, { 'Cache-Control' => 'max-age=400' }, increment_counter]
   end
 
   get '/broken' do
-    [500, { 'Cache-Control' => 'max-age=400' }, "#{settings.requests += 1}"]
+    [500, { 'Cache-Control' => 'max-age=400' }, increment_counter]
   end
 
   get '/get' do
-    [200, { 'Cache-Control' => 'max-age=200' }, "#{settings.requests += 1}"]
+    [200, { 'Cache-Control' => 'max-age=200' }, increment_counter]
   end
 
   get '/private' do
-    [200, { 'Cache-Control' => 'private' }, "#{settings.requests += 1}"]
+    [200, { 'Cache-Control' => 'private' }, increment_counter]
   end
 
   get '/dontstore' do
-    [200, { 'Cache-Control' => 'no-store' }, "#{settings.requests += 1}"]
+    [200, { 'Cache-Control' => 'no-store' }, increment_counter]
   end
 
   get '/expires' do
-    [200, { 'Expires' => (Time.now + 10).httpdate }, "#{settings.requests += 1}"]
+    [200, { 'Expires' => (Time.now + 10).httpdate }, increment_counter]
   end
 
   get '/yesterday' do
-    [200, { 'Date' => settings.yesterday, 'Expires' => settings.yesterday }, "#{settings.requests += 1}"]
+    [200, { 'Date' => settings.yesterday, 'Expires' => settings.yesterday }, increment_counter]
   end
 
   get '/timestamped' do
@@ -60,7 +60,7 @@ class TestApp < Sinatra::Base
     if env['HTTP_IF_MODIFIED_SINCE'] == header
       [304, {}, '']
     else
-      [200, { 'Last-Modified' => header }, "#{settings.requests += 1}"]
+      [200, { 'Last-Modified' => header }, increment_counter]
     end
   end
 
@@ -71,7 +71,12 @@ class TestApp < Sinatra::Base
     if env['HTTP_IF_NONE_MATCH'] == tag
       [304, { 'ETag' => tag, 'Cache-Control' => 'max-age=200', 'Date' => Time.now.httpdate, 'Expires' => (Time.now + 200).httpdate, 'Vary' => '*' }, ""]
     else
-      [200, { 'ETag' => tag, 'Cache-Control' => 'max-age=0', 'Date' => settings.yesterday, 'Expires' => Time.now.httpdate, 'Vary' => 'Accept' }, "#{settings.requests += 1}"]
+      [200, { 'ETag' => tag, 'Cache-Control' => 'max-age=0', 'Date' => settings.yesterday, 'Expires' => Time.now.httpdate, 'Vary' => 'Accept' }, increment_counter]
     end
+  end
+
+  # Increments the 'requests' counter to act as a newly processed response.
+  def increment_counter
+    (settings.requests += 1).to_s
   end
 end
