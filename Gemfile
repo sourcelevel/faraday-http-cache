32655007d096bac183365c1a5b0c7a78a557947c
Lucas Mazza <lucas.mazza@plataformatec.com.br>
2013-11-22 18:22:54 -0200

    Test against RSpec 2.99

--- Gemfile
+++ Gemfile
@@ -6,6 +6,6 @@ gemspec
 gem 'faraday_middleware', '~> 0.9.0'
 gem 'em-http-request',    '~> 1.1.0'
 gem 'sinatra',            '~> 1.4.3'
-gem 'rspec',              '~> 2.14.0'
+gem 'rspec',              '2.99.0.beta1'
 gem 'rake',               '~> 10.1.0'
 gem 'json',               '~> 1.8.0'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,9 +18,7 @@ ENV['FARADAY_ADAPTER'] ||= 'net_http'
 server.start
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
-  config.filter_run :focus
   config.order = 'random'
 
   config.after(:suite) do
